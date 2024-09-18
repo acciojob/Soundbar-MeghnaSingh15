b8b4c77321329ace744d418c37d624380c1bbc78
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-18 23:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('#buttons').forEach(button => {
+document.querySelectorAll('.btn').forEach(button => {
   button.addEventListener('click', () => {
     const sound = button.getAttribute('data-sound');
     const audio = new Audio(`sounds/${sound}.mp3`);
